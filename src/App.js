0a2f3e591199ded907fbf91097abacdaae70d1e7
nstwebd <nst.webd@gmail.com>
2021-04-09 14:41:10 +0530

    create product component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data.json";
+import Products from "./components/Products";
 
-function App() {
+const App = () => {
+  const [products, setPrducts] = useState(data.products);
+  const [size, setSize] = useState("");
+  const [sort, setSort] = useState("");
+
   return (
     <div>
-      <h1>SHOPPING CART APP COMING SOON</h1>
+      <div className="content">
+        <div className="main">
+          <Products products={products} />
+        </div>
+        <div className="side">Cart Items</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
