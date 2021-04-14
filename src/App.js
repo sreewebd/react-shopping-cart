327289ea679184a2994c2d213ee8655346917e23
nstwebd <nst.webd@gmail.com>
2021-04-14 16:28:38 +0530

    changed to classes, implemented sort functionality

--- src/App.js
+++ src/App.js
@@ -1,60 +1,74 @@
-import React, { useState } from "react";
+import React from "react";
 import data from "./data.json";
 import Products from "./components/Products";
 import Filter from "./components/Filter";
 
-const App = () => {
-  const [products, setProducts] = useState(data.products);
-  const [size, setSize] = useState("");
-  const [sort, setSort] = useState("");
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
 
-  const sortProducts = (event) => {
-    console.log(event.target.value);
+  sortProducts = (event) => {
+    const val = event.target.value;
+    this.setState((state) => ({
+      sort: val,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          val === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : val === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        ),
+    }));
   };
 
-  const filterProducts = (event) => {
-    let e = event.target.value;
-    console.log(e);
-    if (e === "") {
-      setProducts({
-        size: e,
-        filteredItems: data.products,
-      });
+  filterProducts = (event) => {
+    // impl
+    console.log(event.target.value);
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products });
     } else {
-      let filteredItems = data.products.filter(
-        (product) => product.availableSizes.indexOf(e) >= 0
-      );
-      console.log("after filteration ", filteredItems);
-      setProducts({
-        // [products]->{{size},[filterdItems]}
-        // ...products,
-        size: e,
-        filteredItems: filteredItems,
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
       });
     }
   };
 
-  return (
-    <div>
-      <div className="content">
-        <div className="main">
-          <Filter
-            count={products.length}
-            size={size}
-            sort={sort}
-            filterProducts={filterProducts}
-            sortProducts={sortProducts}
-          ></Filter>
-          <Products
-            products={
-              products.filteredItems ? products.filteredItems : products
-            }
-          />
+  render() {
+    return (
+      <div>
+        <div className="content">
+          <div className="main">
+            <Filter
+              count={this.state.products.length}
+              size={this.state.size}
+              sort={this.state.sort}
+              filterProducts={this.filterProducts}
+              sortProducts={this.sortProducts}
+            ></Filter>
+            <Products products={this.state.products} />
+          </div>
+          <div className="side">Cart Items</div>
         </div>
-        <div className="side">Cart Items</div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,29 +1,31 @@
 import React from "react";
 import { formatCurrency } from "../util";
 
-const products = ({ products }) => {
-  console.log("products json data ", products);
-  // console.log(Array.isArray(products));
-  return (
-    <div>
-      <ul className="products">
-        {products.map((product) => (
-          <li key={product._id}>
-            <div className="product">
-              <a href={"#" + product._id}>
-                <img src={product.image} alt={product.title} />
-                <p>{product.title}</p>
-              </a>
-              <div className="product-price">
-                <div>{formatCurrency(product.price)}</div>
-                <button className="button primary">Add To Cart</button>
+class Products extends React.Component {
+  render() {
+    // console.log("products json data ", products);
+    // console.log(Array.isArray(products));
+    return (
+      <div>
+        <ul className="products">
+          {this.props.products.map((product) => (
+            <li key={product._id}>
+              <div className="product">
+                <a href={"#" + product._id}>
+                  <img src={product.image} alt={product.title} />
+                  <p>{product.title}</p>
+                </a>
+                <div className="product-price">
+                  <div>{formatCurrency(product.price)}</div>
+                  <button className="button primary">Add To Cart</button>
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
-export default products;
+export default Products;
